ab4d92dc6c059e04c1fd04161b98fcf3268146f7
admin <d.art.shishkin@gmail.com>
2020-07-29 22:50:35 +0300

    Create domain model Item and ItemClientController (67)

---
+++ src/main/java/com/artarkatesoft/learnreactivespring/controllers/ItemClientController.java
@@ -0,0 +1,7 @@
+package com.artarkatesoft.learnreactivespring.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ItemClientController {
+}
---
+++ src/main/java/com/artarkatesoft/learnreactivespring/domain/Item.java
@@ -0,0 +1,14 @@
+package com.artarkatesoft.learnreactivespring.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Item {
+    private String id;
+    private String description;
+    private Double price;
+}
