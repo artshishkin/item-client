5fd5cb783e8248281ea9d2c5f549f0a2e0c48c87
admin <d.art.shishkin@gmail.com>
2020-07-29 23:26:20 +0300

    Explore retrieve() and exchange() in WebClient (68)

--- src/main/java/com/artarkatesoft/learnreactivespring/controllers/ItemClientController.java
+++ src/main/java/com/artarkatesoft/learnreactivespring/controllers/ItemClientController.java
@@ -1,7 +1,47 @@
 package com.artarkatesoft.learnreactivespring.controllers;
 
+import com.artarkatesoft.learnreactivespring.constants.ItemConstants;
+import com.artarkatesoft.learnreactivespring.domain.Item;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Flux;
+
+import javax.annotation.PostConstruct;
 
 @RestController
+@RequiredArgsConstructor
 public class ItemClientController {
+
+    @Value("${itemclient.itemserver.url}")
+    private String itemServerUrl;
+
+    private WebClient webClient;
+
+    @PostConstruct
+    private void init(){
+        webClient = WebClient.create(itemServerUrl);
+    }
+
+    @GetMapping("/client/retrieve")
+    public Flux<Item> getAllItemsUsingRetrieve() {
+        return webClient.get().uri(ItemConstants.ITEM_END_POINT_V1)
+                .accept(MediaType.APPLICATION_JSON)
+                .retrieve()
+                .bodyToFlux(Item.class)
+                .log("Items in Client Project retrieve");
+    }
+
+    @GetMapping("/client/exchange")
+    public Flux<Item> getAllItemsUsingExchange() {
+        return webClient.get().uri(ItemConstants.ITEM_END_POINT_V1)
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange()
+                .flatMapMany(clientResponse->clientResponse.bodyToFlux(Item.class))
+                .log("Items in Client Project exchange");
+    }
+
 }
