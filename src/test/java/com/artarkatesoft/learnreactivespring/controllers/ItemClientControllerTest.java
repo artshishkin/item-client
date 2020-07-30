6554516c6a20010e259d2c7811ed621c2c84ad33
admin <d.art.shishkin@gmail.com>
2020-07-30 10:12:40 +0300

    added dependencies for testing WebClient #3

---
+++ src/test/java/com/artarkatesoft/learnreactivespring/controllers/ItemClientControllerTest.java
@@ -0,0 +1,41 @@
+package com.artarkatesoft.learnreactivespring.controllers;
+
+import okhttp3.mockwebserver.MockWebServer;
+import org.junit.jupiter.api.Test;
+
+class ItemClientControllerTest {
+
+    private final MockWebServer mockWebServer = new MockWebServer();
+
+    @Test
+    void getAllItemsUsingRetrieve() {
+    }
+
+    @Test
+    void getAllItemsUsingExchange() {
+    }
+
+    @Test
+    void getOneItemUsingRetrieve() {
+    }
+
+    @Test
+    void getOneItemUsingExchange() {
+    }
+
+    @Test
+    void postOneItem() {
+    }
+
+    @Test
+    void createItem() {
+    }
+
+    @Test
+    void testCreateItem() {
+    }
+
+    @Test
+    void deleteItem() {
+    }
+}
